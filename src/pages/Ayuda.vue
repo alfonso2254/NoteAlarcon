a5c5c06f7bd0029a0760f1ae180830c65734b406
alfonso2254 <45520382+alfonso2254@users.noreply.github.com>
2020-04-27 01:48:09 -0400

    V1- completed

---
+++ src/pages/Ayuda.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Ayuda</h5>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus ea, temporibus eaque vero sed ratione impedit enim aliquam consequuntur aperiam velit, dolores cum, deserunt recusandae hic placeat at fugit! Enim.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta quod corporis esse quo alias consequatur nobis deserunt sapiente fuga adipisci, impedit perferendis, cumque voluptas atque provident quaerat! Sint, neque quasi?</p>
+  </q-page>
+</template>
+
+<script>
+export default {};
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,20 +1,22 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: "", component: () => import("pages/Todos.vue") },
+      { path: "/help", component: () => import("pages/Ayuda.vue") },
+      { path: "/info", component: () => import("pages/Informacion.vue") }
+
     ]
   }
-]
+];
 
 // Always leave this as last one
-if (process.env.MODE !== 'ssr') {
+if (process.env.MODE !== "ssr") {
   routes.push({
-    path: '*',
-    component: () => import('pages/Error404.vue')
-  })
+    path: "*",
+    component: () => import("pages/Error404.vue")
+  });
 }
 
-export default routes
+export default routes;
